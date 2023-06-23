3bfd34941d5cc574462a2791e69e03a463bf4c47
lukeleech <lukeyoobee@gmail.com>
2023-06-23 14:50:19 +1200

    Deployment

--- users/models.py
+++ users/models.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    username = models.CharField(max_length=255, default='')
+    username = models.CharField(max_length=255, default='',unique=True)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     logged_in = models.BooleanField(default=False)
     
-    #USERNAME_FIELD = 'email'
+    #change this back once superuser is made and remove unique from username
+    #USERNAME_FIELD = 'email' 
     #REQUIRED_FIELDS = []
