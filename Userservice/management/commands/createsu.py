bf32c50a385f35a5719da4a83868c44d4fd97b6b
lukeleech <lukeyoobee@gmail.com>
2023-06-25 22:29:29 +1200

    Deployment attemt 3 SU creation

--- Userservice/management/commands/createsu.py
+++ Userservice/management/commands/createsu.py
@@ -1,18 +1,24 @@
 from django.core.management.base import BaseCommand
 from users.models import User
 from django.contrib.auth.management.commands import createsuperuser
-
-class Command(createsuperuser.Command):
+import requests
+class Command(BaseCommand):
 
    def handle(self, *args, **options):
         # Check if a user record with the username 'awsadmin' exists in the database
         # If it does not exist, then create a super-user with that username,
         # Supply an email address and a password to it
         # So, now we have an admin user to manage our AWS RDS database
-        if not User.objects.filter(username="awsadmin").exists():
-            username="awsadmin"
-            email="lukeyoobee@gmail.com"
-            password="AWSAdmin123"
+        """if not User.objects.filter(username="awsadmin").exists():
+            User.objects.create_superuser("awsadmin", "lukeyoobee@gmail.com", "AWSAdmin123")"""
             
-            User.objects.create(username, email, password)
-            
+        url = 'http://kiwinco-userapi-dev.ap-southeast-2.elasticbeanstalk.com/api/create/'  # Replace with your API endpoint
+        data = {
+            'username': "awsadmin",
+            'email': "lukeyoobee@gmail.com",
+            'password': "AWSAdmin123",
+            'is_staff': 'True',
+            'is_superuser': 'True',
+        }
+        requests.post(url, data=data)
+        
