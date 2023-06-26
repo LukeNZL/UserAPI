34e1541d59708c949a8f1b64ea8115b3c1067615
lukeleech <lukeyoobee@gmail.com>
2023-06-26 14:47:00 +1200

    deployment attempt

--- Userservice/settings.py
+++ Userservice/settings.py
@@ -195,7 +195,7 @@ else:
     STATIC_URL = '/static/'
     #STATIC_ROOT = 'static'
 
-    # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
