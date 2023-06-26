cc51b01d042d47052ffdbe8dd2e5707951bb9145
lukeleech <lukeyoobee@gmail.com>
2023-06-26 14:12:58 +1200

    Updated S3 settings for staticfile colleciton

--- Userservice/settings.py
+++ Userservice/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'storages',
     'users',
     'gunicorn',
 ]
@@ -140,8 +141,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
+#STATIC_URL = 'static/'
+#STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -156,3 +157,44 @@ AUTH_USER_MODEL = 'users.User'
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+
+if 'S3_BUCKET' in os.environ:
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    print("IS IT HERE??")
+    print(AWS_ACCESS_KEY_ID)
+
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    print("IS IT HERE??")
+    print(AWS_SECRET_ACCESS_KEY)
+
+    AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET']
+    print("IS IT HERE??")
+    print(AWS_STORAGE_BUCKET_NAME)
+
+    AWS_S3_REGION_NAME = 'ap-southeast-2'
+
+    #AWS_DEFAULT_ACL = 'public-read'
+    AWS_DEFAULT_ACL = None
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+    print("IS IT HERE??")
+    print(AWS_S3_CUSTOM_DOMAIN)
+
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    AWS_LOCATION = 'static'
+
+    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    print("IS IT HERE??")
+    print(STATIC_URL)
+
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+else:   
+    STATIC_URL = '/static/'
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
