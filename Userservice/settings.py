9e059b4d6caa8670eca9b5dfe09c0c0cf0324b1e
lukeleech <lukeyoobee@gmail.com>
2023-06-26 15:54:31 +1200

    deployment, working static but now removing access codes from being hardcoded

--- Userservice/settings.py
+++ Userservice/settings.py
@@ -180,8 +180,8 @@ if 'S3_BUCKET' in os.environ:
     print(STATIC_URL)
 
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'"""
-    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    AWS_S3_ACCESS_KEY_ID = os.environ['AWS_S3_ACCESS_KEY_ID']
+    AWS_S3_SECRET_ACCESS_KEY = os.environ['AWS_S3_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET']
     
     #AWS_STORAGE_BUCKET_NAME = 'kiwinco-api-bucket'
