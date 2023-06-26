2f80a8ff91a7dc8ec8f26d9eb7c0d3571ef19ce0
lukeleech <lukeyoobee@gmail.com>
2023-06-26 15:07:58 +1200

    deployment attempt

--- Userservice/urls.py
+++ Userservice/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -26,4 +27,4 @@ urlpatterns = [
     
     
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import UserList, UserCreate, UserDelete, UserLogin,UserEdit,LogoutView, UserView
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', UserList.as_view(), name='UserList'),
@@ -15,4 +17,4 @@ urlpatterns = [
    
    
     # Add other URLs for different views
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
