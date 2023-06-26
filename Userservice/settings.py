00e3113e5d0fcf8f72d7ebf5bb8b4a461db76c1b
lukeleech <lukeyoobee@gmail.com>
2023-06-26 16:00:06 +1200

    deployment

--- Userservice/settings.py
+++ Userservice/settings.py
@@ -182,9 +182,9 @@ if 'S3_BUCKET' in os.environ:
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'"""
     #AWS_S3_ACCESS_KEY_ID = os.environ['AWS_S3_ACCESS_KEY_ID']
     #AWS_S3_SECRET_ACCESS_KEY = os.environ['AWS_S3_SECRET_ACCESS_KEY']
-    AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET']
+    #AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET']
     
-    #AWS_STORAGE_BUCKET_NAME = 'kiwinco-api-bucket'
+    AWS_STORAGE_BUCKET_NAME = 'kiwinco-api-bucket'
     AWS_S3_REGION_NAME = 'ap-southeast-2'
 
     AWS_S3_ACCESS_KEY_ID = 'AKIA4TMQ4WVOPZAHTMP2'
