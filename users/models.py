3b9cff538bfe4e9d545b2ad0a8a914e41249a53b
lukeleech <lukeyoobee@gmail.com>
2023-06-26 14:41:39 +1200

    deployment attempt

--- users/models.py
+++ users/models.py
@@ -7,9 +7,9 @@ class User(AbstractUser):
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     logged_in = models.BooleanField(default=False)
-    #is_admin = models.BooleanField(default=True)
-    #is_staff = models.BooleanField(default=True)
-    #is_superuser = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     #change this back once superuser is made and remove unique from username
     USERNAME_FIELD = 'email' 
     REQUIRED_FIELDS = []
